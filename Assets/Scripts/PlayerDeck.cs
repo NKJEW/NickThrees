105afd43f37b1db1a6a8320e168bafca9e3ec87d
Nathaniel Kiyasu Jew <njew25@berkeley.edu>
2020-03-17 22:41:01 -0700

    Player deck file

---
+++ Assets/Scripts/PlayerDeck.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeck : MonoBehaviour
+{
+    //representation of your hand
+    //representation of your visible cards (face up ones)
+    //representation of your face down cards
+    //which cards in your hand are playable given the current center pile
+    //
+}
