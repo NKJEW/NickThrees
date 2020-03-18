8d9c9d168a0bcae6c6fb4a01ad2d106c75a3ee3b
Nathaniel Kiyasu Jew <njew25@berkeley.edu>
2020-03-17 22:04:20 -0700

    Added draw pile

---
+++ Assets/Scripts/DrawPile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawPile : MonoBehaviour {
+    List<CardData> pile;
+
+    public void Init(List<CardData> cards) {
+        pile = cards;
+    }
+
+    public bool IsEmpty() {
+        return pile.Count == 0;
+    }
+
+    public CardData DrawCard() {
+        CardData newCard = pile[pile.Count - 1];
+        pile.Remove(newCard);
+        return newCard;
+    }
+}
